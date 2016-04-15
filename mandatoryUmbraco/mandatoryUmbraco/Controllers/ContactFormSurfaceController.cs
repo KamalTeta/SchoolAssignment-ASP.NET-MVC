e7f5d394eedaa6cc849ff93691895c82a1918bd1
TalkativeTRex <talkativetrex@gmail.com>
2016-04-15 05:23:31 +0200

    putting the project online

---
+++ mandatoryUmbraco/mandatoryUmbraco/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+using mandatoryUmbraco.ViewModels;
+using System.Net.Mail;
+
+namespace mandatoryUmbraco.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+
+        public ActionResult Index()
+        {
+            return PartialView("contactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+
+
+            IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Message");
+
+            comment.SetValue("cName", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("message", model.Message);
+
+            //Save
+            Services.ContentService.Save(comment);
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
